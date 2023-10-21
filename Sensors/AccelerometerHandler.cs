560d550ecb15fd0f3efbc849b1ff6bb9a06ec5e4
Adam <Adam@DESKTOP-R5R7J0B>
2023-10-21 17:44:25 +0200

    Refaktoryzacja kodu: dodanie specjalnego folderu na obsługę sensorów.

---
+++ Sensors/AccelerometerHandler.cs
@@ -0,0 +1,44 @@
+﻿using Android.Content;
+using Android.Hardware;
+using Android.Runtime;
+using Android.Widget;
+using System;
+
+public class AccelerometerHandler : Java.Lang.Object, ISensorEventListener
+{
+    private SensorManager _sensorManager;
+    private TextView _accelerometerDataTextView;
+
+    public AccelerometerHandler(SensorManager sensorManager, TextView accelerometerDataTextView)
+    {
+        _sensorManager = sensorManager;
+        _accelerometerDataTextView = accelerometerDataTextView;
+    }
+
+    public void StartListening()
+    {
+        _sensorManager.RegisterListener(this, _sensorManager.GetDefaultSensor(SensorType.Accelerometer), SensorDelay.Normal);
+    }
+
+    public void StopListening()
+    {
+        _sensorManager.UnregisterListener(this);
+    }
+
+    public void OnAccuracyChanged(Sensor sensor, [GeneratedEnum] SensorStatus accuracy)
+    {
+        // Do something when accuracy of the sensor changes
+    }
+
+    public void OnSensorChanged(SensorEvent e)
+    {
+        if (e.Sensor.Type == SensorType.Accelerometer)
+        {
+            double x = e.Values[0];
+            double y = e.Values[1];
+            double z = e.Values[2];
+
+            _accelerometerDataTextView.Text = $"X: {x}\nY: {y}\nZ: {z}";
+        }
+    }
+}
---
+++ Sensors/AudioRecorder.cs
@@ -0,0 +1,78 @@
+﻿using Android.Media;
+using System;
+using System.Threading.Tasks;
+using Android.Widget;
+
+public class AudioRecorder
+{
+    private AudioRecord _audioRecord;
+    private bool _isRecording;
+    private TextView _volumeLevelTextView;
+
+    public AudioRecorder(TextView volumeLevelTextView)
+    {
+        _volumeLevelTextView = volumeLevelTextView;
+    }
+
+    public void StartRecording()
+    {
+        int minBufferSize = AudioRecord.GetMinBufferSize(44100, ChannelIn.Mono, Encoding.Pcm16bit);
+        _audioRecord = new AudioRecord(AudioSource.Mic, 44100, ChannelIn.Mono, Encoding.Pcm16bit, minBufferSize);
+
+        try
+        {
+            _audioRecord.StartRecording();
+            _isRecording = true;
+
+            Task.Run(() => ReadAudioData());
+        }
+        catch (Exception ex)
+        {
+            // Obsługa wyjątku
+            Console.WriteLine("Exception during audio recording: " + ex.Message);
+        }
+    }
+
+    public void StopRecording()
+    {
+        if (_isRecording)
+        {
+            _isRecording = false;
+            _audioRecord.Stop();
+            _audioRecord.Release();
+            _audioRecord = null;
+        }
+    }
+
+    private void ReadAudioData()
+    {
+        byte[] buffer = new byte[1024];
+        while (_isRecording)
+        {
+            int bytesRead = _audioRecord.Read(buffer, 0, buffer.Length);
+            if (bytesRead > 0)
+            {
+                double volumeLevel = CalculateVolumeLevel(buffer, bytesRead);
+                DisplayVolumeLevel(Math.Round(volumeLevel));
+            }
+        }
+    }
+
+    private double CalculateVolumeLevel(byte[] audioData, int length)
+    {
+        int sum = 0;
+        for (int i = 0; i < length; i += 2)
+        {
+            short sample = (short)((audioData[i + 1] << 8) | audioData[i]);
+            sum += Math.Abs(sample);
+        }
+
+        double averageAmplitude = (double)sum / (length / 2);
+        return averageAmplitude;
+    }
+
+    private void DisplayVolumeLevel(double volumeLevel)
+    {
+        _volumeLevelTextView.Text = $"Volume Level: {Math.Round(volumeLevel, 3)}";
+    }
+}
